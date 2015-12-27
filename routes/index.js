e8d0d6f056c91318d54757f56ff788c8854f788a
Stuwert <stuart@urback.net>
2015-12-26 21:11:14 -0800

    Complete exercise

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var hipstered = require('../lib/snippets');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,8 +8,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.post("/foo/bar", function(req, res, next){
-  req.body;
-  res.render('index', req.body);
+  var userinput = req.body["userinput"].split(' ');
+  var hipsteroutput = userinput.map(function(item){
+    return item + ' ' + randomHipster();
+  })
+  var output = {
+    output: hipsteroutput,
+  }
+  res.render('hipstered', output);
 })
+
+function randomHipster(){
+  var randHipstnum = +(Math.random() * (hipstered.length - 1)).toFixed(0);
+  return hipstered[randHipstnum];
+}
 
 module.exports = router;
